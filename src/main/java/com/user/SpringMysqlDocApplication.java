25ee084e5e600ade5fb6d79883b9555cd2315d63
Piyush <piyushsinghlkr2002@gmail.com>
2024-05-01 19:11:21 +0530

    first commit

---
+++ src/main/java/com/user/SpringMysqlDocApplication.java
@@ -0,0 +1,13 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlDocApplication.class, args);
+	}
+
+}
